0df87f65a661827d719d3c8bcfc222ef598737ae
Whit3eo <jockel121.121@gmail.com>
2022-11-17 14:45:36 +0100

    d

--- Flutter_app/flutter_homealarm/lib/Scheme_page.dart
+++ Flutter_app/flutter_homealarm/lib/Scheme_page.dart
@@ -11,8 +11,8 @@ class SchemePage extends StatefulWidget {
 class _SchemePageState extends State<SchemePage> {
   DateTime _dateTime = DateTime.now();
 
-  String setHour = "00";
-  String setMinute = "00";
+  String startTime = "00:00";
+  String endTime = "00:00";
 
   @override
   Widget build(BuildContext context) {
@@ -30,60 +30,70 @@ class _SchemePageState extends State<SchemePage> {
       body: Column(
         children: [
           Container(
-              margin: const EdgeInsets.only(top: 30,),
+              margin: const EdgeInsets.only(
+                top: 30,
+              ),
               child: Text("Your scheduled time: ${setHour}:${setMinute}")),
-            
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  margin: const EdgeInsets.all(20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  margin: const EdgeInsets.only(top: 20, right: 40),
                   child: TimePickerSpinner(
                     is24HourMode: true,
                     normalTextStyle:
-                    const TextStyle(fontSize: 24, color: Colors.lime),
+                        const TextStyle(fontSize: 24, color: Colors.lime),
                     highlightedTextStyle:
-                    const TextStyle(fontSize: 24, color: Colors.black),
+                        const TextStyle(fontSize: 24, color: Colors.black),
                     spacing: 20,
                     itemHeight: 80,
                     isForce2Digits: true,
                     onTimeChange: (time) {
-                    setState(() {
-                      setCurrentTime(time);
-                    });
+                      setState(() {
+                        setStartTime(time);
+                      });
                     },
-                  )
-                ),
-                Container(
-                  margin: const EdgeInsets.all(20),
+                  )),
+              Container(
+                  margin: const EdgeInsets.only(top: 20, left: 40),
                   child: TimePickerSpinner(
                     is24HourMode: true,
                     normalTextStyle:
-                    const TextStyle(fontSize: 24, color: Colors.blue),
+                        const TextStyle(fontSize: 24, color: Colors.blue),
                     highlightedTextStyle:
-                    const TextStyle(fontSize: 24, color: Colors.black),
+                        const TextStyle(fontSize: 24, color: Colors.black),
                     spacing: 20,
                     itemHeight: 80,
                     isForce2Digits: true,
                     onTimeChange: (time) {
-                    setState(() {
-                      setCurrentTime(time);
-                    });
+                      setState(() {
+                        setEndTime(time);
+                      });
                     },
-                  )
-                ),
-              ],
-            ),
-          
-          Container(
-            margin: const EdgeInsets.only(top: 10),
-            child: Text(
-              "${setHour}:${setMinute}",
-              textScaleFactor: 3,
-              style: TextStyle(color: Colors.blue),
-            ),
+                  )),
+            ],
           ),
-          
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 10, right: 20),
+                child: Text(
+                  startTime,
+                  textScaleFactor: 4,
+                  style: const TextStyle(color: Colors.lime),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 10, left: 20),
+                child: Text(
+                  endTime,
+                  textScaleFactor: 4,
+                  style: TextStyle(color: Colors.blue),
+                ),
+              ),
+            ],
+          ),
           Container(
               margin: const EdgeInsets.only(top: 50),
               height: 80,
@@ -91,9 +101,8 @@ class _SchemePageState extends State<SchemePage> {
               child: MaterialButton(
                 onPressed: () {
                   print("Set schedule");
-                  ;
                 },
-                shape: StadiumBorder(),
+                shape: const StadiumBorder(),
                 color: Colors.grey,
                 child: const Text("Set scheme"),
               ))
@@ -106,7 +115,29 @@ class _SchemePageState extends State<SchemePage> {
   Adds a 0 infront of hour or minute 
   if they are < 10                 
   */
-  void setCurrentTime(DateTime time) {
+  void setStartTime(DateTime time) {
+    String setHour = "";
+    String setMinute = "";
+
+    if (time.hour.toInt() < 10) {
+      setHour = "0" + time.hour.toString();
+    } else {
+      setHour = time.hour.toString();
+    }
+
+    if (time.minute.toInt() < 10) {
+      setMinute = "0" + time.minute.toString();
+    } else {
+      setMinute = time.minute.toString();
+    }
+
+    startTime = "$setHour:$setMinute";
+  }
+
+  setEndTime(DateTime time) {
+    String setHour = "";
+    String setMinute = "";
+
     if (time.hour.toInt() < 10) {
       setHour = "0" + time.hour.toString();
     } else {
@@ -119,4 +150,5 @@ class _SchemePageState extends State<SchemePage> {
       setMinute = time.minute.toString();
     }
   }
+  
 }
